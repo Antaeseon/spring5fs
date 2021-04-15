ba0f6823e5c985e168e45f51670e464e120ec5c7
Antaeseon <ants412@naver.com>
2021-04-16 01:59:54 +0900

    make register form

--- sp5-chap11/src/main/java/config/MemberConfig.java
+++ sp5-chap11/src/main/java/config/MemberConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import spring.ChangePasswordService;
 import spring.MemberDao;
 import spring.MemberRegisterService;
-
 
 @Configuration
 @EnableTransactionManagement
@@ -31,27 +31,26 @@ public class MemberConfig {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(){
+    public PlatformTransactionManager transactionManager() {
         DataSourceTransactionManager tm = new DataSourceTransactionManager();
         tm.setDataSource(dataSource());
         return tm;
     }
 
     @Bean
-    public MemberDao memberDao(){
+    public MemberDao memberDao() {
         return new MemberDao(dataSource());
     }
 
     @Bean
-    public MemberRegisterService memberRegSvc(){
+    public MemberRegisterService memberRegSvc() {
         return new MemberRegisterService(memberDao());
     }
 
     @Bean
-    public ChangePasswordService changePwdSvc(){
+    public ChangePasswordService changePwdSvc() {
         ChangePasswordService pwdSvc = new ChangePasswordService();
         pwdSvc.setMemberDao(memberDao());
         return pwdSvc;
     }
-
 }
